610a7c438ac1ebd0f5becbddcdd3f42add1c6c4b
Linda Marcela Guillen Abarca <Linda.Guillen231219@liveusam.edu.sv>
2025-02-28 21:19:08 -0600

    prueba

--- AplicacionP/Program.cs
+++ AplicacionP/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// Prueba para el parcial 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
